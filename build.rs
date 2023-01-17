2bd3b340b01aa962eeed8d65dff22c877b89a20b
Xuejie Xiao <xxuejie@gmail.com>
2023-01-17 10:18:18 +0800

    feat: Use vendored protoc in case running on a fresh machine (#1)

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ extern crate protoc_rust;
 
 fn main() {
     protoc_rust::Codegen::new()
+        .protoc_path(protoc_bin_vendored::protoc_bin_path().unwrap())
         .out_dir("src/protos")
         .inputs(&["proto/profile.proto"])
         .include("proto")
